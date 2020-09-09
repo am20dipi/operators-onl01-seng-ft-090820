77de8c9d8a36b85743f5cc1fc6b9a3cd4590aa79
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-09 17:17:30 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    true 
+  else speed > 40 || speed < 95
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed == 50 ? true : false
+	speed < 40 ? true : false || speed > 60 ? true : false
 end
 	
 
